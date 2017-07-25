3a3706ff48400a922912e34fa5c7dae2d2fb098b
DK\ili <ili@sitecore.net>
2017-07-25 18:47:37 +0300

    Release

---
+++ src/Sitecore.Support.166899/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.166899")]
+[assembly: AssemblyProduct("Sitecore.Support.166899")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.166899/ToggleLockRequest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Sitecore.Data.Items;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.Web;
+using Sitecore.ExperienceEditor.Speak.Ribbon.Requests.LockItem;
+using Sitecore.Links;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LockItem
+{
+    public class ToggleLockRequest : PipelineProcessorRequest<ItemContext>
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            base.RequestContext.ValidateContextItem();
+            Sitecore.Data.Items.Item item = this.SwitchLock(base.RequestContext.Item);
+            this.HandleVersionCreating(item);
+            return new PipelineProcessorResponseValue
+            {
+                Value = new
+                {
+                    Locked = item.Locking.IsLocked(),
+                    Version = item.Version.Number
+                }
+            };
+        }
+
+        protected Sitecore.Data.Items.Item SwitchLock(Sitecore.Data.Items.Item item)
+        {
+            if (item.Locking.IsLocked())
+            {
+                ItemLink[] itemLinks = item.Links.GetValidLinks();
+                item.Locking.Unlock();
+                foreach (ItemLink link in itemLinks)
+                {
+                    Item targetItem = link.GetTargetItem();
+                    if (targetItem == null || !targetItem.Locking.IsLocked()) continue;
+                    targetItem.Locking.Unlock();
+                }
+                return item;
+            }
+            if (Sitecore.Context.User.IsAdministrator)
+            {
+                item.Locking.Lock();
+                return item;
+            }
+            return Sitecore.Context.Workflow.StartEditing(item);
+        }
+
+        private void HandleVersionCreating(Sitecore.Data.Items.Item finalItem)
+        {
+            if (base.RequestContext.Item.Version.Number != finalItem.Version.Number)
+            {
+                Sitecore.Web.WebUtil.SetCookieValue(base.RequestContext.Site.GetCookieKey("sc_date"), string.Empty, DateTime.MinValue);
+            }
+        }
+    }
+}
